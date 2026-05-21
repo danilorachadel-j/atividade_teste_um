79ca7e45ba28968c1a1b0ca39f9a047264cf6a44
Danilorachadel-j <danilo_rachadel-j@>
2026-05-21 09:21:55 -0300

    add - adicionei mais informações

--- index.php
+++ index.php
@@ -6,5 +6,13 @@
 </head>
 <body>
     <h1>Tela de Login - PHP</h1>
+    <form method="POST">
+    <label>Usuário</label>
+    <input type="text" name="usuario"> <br>
+    <label>senha</label>
+    <input type="password" name="senha"><br>
+    
+    <button type="submit">Entrar</button>
+    </form>
 </body>
 </html>
